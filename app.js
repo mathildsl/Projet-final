ca607e206d131aaf07f7049c8b1fb3841c081b30
mathildsl <mathildsl@users.noreply.github.com>
2017-11-28 16:15:10 +0100

    Projet-final

---
+++ app.js
@@ -0,0 +1,68 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    quiz: quiz, // Références aux questions du quiz
+    current: 0, // Numéro de la question en cours
+    success: null, // null si pas de réponse, true si succès, false si erreur
+    score: 0 // Incrémentation du score final
+  },
+  computed: {
+    step: function step () { 
+      // Renvoie l'objet de la question en cours
+      return this.quiz[this.current];
+    }
+  },
+  methods: {
+    submitButton: function submitButton (answer) {
+      // Modifie la propriété succès en fonction de la réponse (QCM)
+      var correction = this.step.choices[this.step.answer];
+      this.step.userAnswer = this.step.choices.indexOf(answer)
+
+      if (answer === correction) {
+      	this.success = true;
+      } else {
+      	this.success = false;
+      }
+    },
+    submitText: function submitText () {
+      // Modifie la propriété succès en fonction de la réponse (réponse libre)
+      var answer = this.$refs.textInput.value;
+      var correction = this.step.answer;
+
+      if (answer === correction) {
+      	this.success = true;
+      } else {
+      	this.success = false;
+      }
+    },
+    nextQuestion: function nextQuestion () {
+      // Incrémente le score, réinitialise la propriété succès, passe à la question suivante
+      if (this.success === true) {
+      	this.score++;
+      }
+      this.success = null;
+      this.current++;
+    },
+
+    buttonClasses: function buttonClasses (value) {
+      // Renvoie les classes pour colorer les bouttons QCM en fonction de la réponse
+      // Ne pas modifier :)
+      var correction = this.step.choices[this.step.answer];
+      if (this.success === true) {
+        if (correction === value) {
+          return 'btn-success';
+        } else {
+          return ''
+        }
+      }
+      if (this.success === false) {
+        if (correction === value) {
+          return 'btn-outline-success';
+        } else {
+          return 'btn-outline-danger';
+        }
+      }
+      return 'btn-outline-dark';
+    }
+  }
+});
---
+++ quiz.js
@@ -0,0 +1,42 @@
+var quiz = [
+  {
+    image: './images/citeu.jpg',
+    question: "Dans quel arrondissement se trouve la station Cité Universitaire ?",
+    choices: ["le 13e", "le 14e", "le 12e"],
+    explainer: ["La station se trouve dans le 14e arrondissement. Elle se trouvait auparavant sur la ligne de chemin de fer qui reliait Denfert-Rochereau à la ville de Sceaux.", "Elle se trouvait auparavant sur la ligne de chemin de fer qui reliait Denfert-Rochereau à la ville de Sceaux.", "La station se trouve dans le 14e arrondissement. Elle se trouvait auparavant sur la ligne de chemin de fer qui reliait Denfert-Rochereau à la ville de Sceaux."],
+    answer: 1
+  },
+  {
+    image: './images/gdn.jpg',
+    question: "Quelle station de métro est la plus fréquentée du réseau ?",
+    choices: ["Gare de Lyon", "Châtelet", "Gare du Nord"],
+    explainer: ["La Gare de lyon n'a accueilli que 37 millions de personnes en 2016. La Gare du Nord, elle, en a brassé plus de 50 millions.", "Les Halles n'a accueilli que 16 millions de personnes en 2016. La Gare du Nord, elle, en a brassé plus de 50 millions.", "La Gare du Nord a brassé plus de 50 millions de personnes en 2016. Elle si situe donc à la tête des stations les plus empruntées, devant la gare Saint Lazare et ses 45 millions de voyageurs, et la Gare de Lyon, qui plafonne à un peu plus de 36 millions."],
+    answer: 2
+  },
+   {
+    image: './images/abbesses.jpg', 
+    question: "A combien de mètres la station Abbesses s'enfonce-t-elle sous terre ?",
+    choices: ["36", "39", "42"],
+    explainer: ["C'est la station la plus profonde du réseau !","La station la plus profonde du réseau s'enfonce à 36 mètres.", "La station la plus profonde du réseau s'enfonce à 36 mètres." ],
+    answer: 0
+  },
+  {
+    image: './images/molitor.jpg', 
+    question: "Sur la ligne 9, il y a deux stations Michel-Ange. L'une s'appelle Michel Ange Auteuil et la seconde Michel Ange ... ",
+    explainer: "Du nom de la rue sur laquelle donne la station.",
+    answer: "Molitor"
+  },
+  {
+    image: './images/cluny.jpg', 
+    question: "A pieds, combien de minutes séparent la station Cluny la Sorbonne de Maubert Mutualité ?",
+    choices: ["3", "5", "7"],
+    explainer: ["Faire le trajet en métro prendrait presque plus de temps.","Il faut seulement 3 minutes pour rallier les deux stations. Faire le trajet en métro prendrait presque plus de temps.", "Il faut seulement 3 minutes pour rallier les deux stations. Faire le trajet en métro prendrait presque plus de temps."],
+    answer: 0
+  },
+  {
+    image: './images/roosevelt.jpg',
+    question: "Quel est le nom exact de la station Roosevelt, sur la ligne 1 ?'",
+    explainer: "Franklin D. Roosevelt, pour Franklin Delano Roosevelt, le 32e président des Etats-Unis. La station a été nommée en l'honneur du chef d'Etat d'un pays allié pendant la 2e Guerre mondiale.",
+    answer: "Franklin D. Roosevelt"
+  }
+];
